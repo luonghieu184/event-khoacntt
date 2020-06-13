2c5373aa688808389969f212b54e7010c6161da6
luonghieu184 <luonghieu184@gmail.com>
2020-06-13 12:21:05 +0700

    add css index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
+import { ViewEventComponent } from './components/view-event/view-event.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'create-event', component: CreateEventComponent}
+  { path: 'create-event', component: CreateEventComponent},
+  { path: 'view-event', component: ViewEventComponent}
 
 ];
 
