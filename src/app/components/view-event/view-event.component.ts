afbecb51526ffebb5e926d5a99e66ccadddbf990
luonghieu184 <luonghieu184@gmail.com>
2020-06-13 13:03:12 +0700

    View event url id

--- src/app/components/view-event/view-event.component.ts
+++ src/app/components/view-event/view-event.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  HttpClientModule,
+  HttpClient,
+  HttpHeaders,
+  HttpParams
+} from "@angular/common/http";
 @Component({
   selector: 'app-view-event',
   templateUrl: './view-event.component.html',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEventComponent implements OnInit {
 
-  constructor() { }
+  apiRoot: string = "https://script.google.com/macros/s/AKfycbzp1brXop99zVGaqSehBVQlO0c5s8b-0fKj4UzPUxx01DkoJWuF/exec?request";
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  getLastWord(phrWord){
+    var nSpace = phrWord.split("-");
+    return nSpace[nSpace.length - 1];
+  } 
+  ngOnInit() {
+   var eventID = location.search.split("=")[1];
+   var getEventID = this.getLastWord(eventID);
+   if(getEventID != ""){
+    this.doGetEV(getEventID);
+   }
+  }
+  evID : any;
+  evTitle:any;
+  evDescription:any;
+  evContent:any;
+  evDateCreate:any;
+  evStatus :any;
+
+
+  doGetEV(id){
+    console.log("GET EVENT");
+    let url = `${this.apiRoot}=getEventID&evID=`+id;
+    const httpOptions = {
+      params: new HttpParams().set("foo", "moo").set("limit", "25")
+    };
+    this.http.get(url, httpOptions).subscribe(
+      (res: any) => {console.log(res)
+        this.evID = res.evID; 
+        // this.evTitle = res.evTitle; 
+        // this.evDescription = res.evDescription; 
+        this.evContent = res.evContent; 
+        // this.evDateCreate = res.evDateCreate; 
+        // this.evStatus = res.evStatus; 
+      });
+
   }
 
 }
